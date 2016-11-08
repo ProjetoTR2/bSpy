d00a1a70448c85b041f8eee0c8ff5a85772119e0
edusn <edsnanami@gmail.com>
2016-11-08 03:54:43 -0200

    Adicionando campo de data no model image

--- bSpy/db/schema.rb
+++ bSpy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161106050225) do
+ActiveRecord::Schema.define(version: 20161108051835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,9 +19,10 @@ ActiveRecord::Schema.define(version: 20161106050225) do
   create_table "images", force: :cascade do |t|
     t.string   "name"
     t.binary   "data"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.binary   "file"
+    t.datetime "data_de_criacao"
   end
 
   create_table "users", force: :cascade do |t|
